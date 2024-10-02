8c01615c9f9539524ffa67c2748bc97e89281d7c
Sam <user1@localhost.com>
2024-10-02 19:17:01 +0100

    Get buttons and operations to work with display

--- calculator/app.js
+++ calculator/app.js
@@ -1,7 +1,10 @@
+// NOTE: I tried to follow the instructions as best as my understanding and ability allow me.
+// This being said, I'd do some things differently.
+
 let number1;
 let operator;
 let number2;
-let displayValue;
+let displayValue = "";
 
 function add(num1, num2) {
   return num1 + num2;
@@ -19,30 +22,61 @@ function divide(num1, num2) {
   return num1 / num2;
 }
 
-function operate(num1, operator, num2) {
-  if (operator === "+") {
-    return add(num1, num2);
+function operate() {
+    displayValue = display.value;
+    operator = /\+|-|x|\//.exec(displayValue);
+    let operationNumbers = displayValue.split(operator).map(element => Number(element));
+    
+  if (operator == "+") {
+    display.value = add(operationNumbers[0], operationNumbers[1]);
   }
-  if (operator === "-") {
-    return subtract(num1, num2);
+  if (operator == "-") {
+    display.value = subtract(operationNumbers[0], operationNumbers[1]);
   }
-  if (operator === "*") {
-    return multiply(num1, num2);
+  if (operator == "x") {
+    display.value = multiply(operationNumbers[0], operationNumbers[1]);
   }
-  if (operator === "/") {
-    return divide(num1, num2);
+  if (operator == "/") {
+    display.value = divide(operationNumbers[0], operationNumbers[1]);
   }
 }
 
-function appendToDisplay() {
-    return null;
+const display = document.getElementById("display");
+display.value = displayValue;
+
+let clickedButton = null;
+const buttons = document.querySelectorAll('.operator-button, #decimal');
+
+// Add event listener to each button
+buttons.forEach(button => {
+    button.addEventListener('click', function(event) {
+        handleButtonClick(event);
+    });
+});
+
+function handleButtonClick(event) {
+    // "event.target" refers to the button that was clicked
+    clickedButton = event.target;
+    console.log("Button clicked:", clickedButton);
 }
 
-function clear() {
-  return (displayValue = 0);
+function appendNumber(input) {
+    display.value += input;
 }
 
-console.log(operate(2, "+", 2))
-console.log(operate(2, "-", 2))
-console.log(operate(2, "*", 2))
-console.log(operate(2, "/", 2))
+function appendOperator(input) {
+    display.value += input;
+    // If there's an operator in display, call operate
+    // Enable decimal button
+    // Disable other operators (except =; change css)
+}
+
+function appendDecimal(input) {
+    display.value += input;
+    // Disable button
+    clickedButton.setAttribute("disabled", "disabled");
+}
+
+function clearDisplay() {
+    display.value = "";
+}
